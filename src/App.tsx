5c5440037a8f8a9daf663bddb45fbf891fce462c
EmanSan9 <sanna950@gmail.com>
2025-08-08 09:14:23 +0200

    V1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HomePage } from "./components/HomePage";
 import { ArticleDetail } from "./components/ArticleDetailPage";
 
-const App: React.FC = () => (
+const App = () => (
   <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
